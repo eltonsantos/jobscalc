bf4d3ccc523d81765f77145cb59a7b6a28325804
Elton Santos <eltonaxl@hotmail.com>
2024-09-23 00:25:25 -0300

    Almost finish

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script>
-  import "./app.css"
-  import Header from "./home/Header.svelte";
+  import "./app.css";
+  import Home from "./home/Home.svelte";
+  import Profile from "./profile/Profile.svelte";
+  import Project from "./project/Project.svelte";
+  import { app } from "./store";
 </script>
 
-<Header />
+{#if $app.page == "home"}
+  <Home />
+{:else if $app.page == "profile"}
+  <Profile />
+{:else}
+  <Project />
+{/if}
